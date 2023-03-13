3a454e891a905d2045f9b7bb511ae5f5e9d2e895
IT22550262 <127716546+IT22550262@users.noreply.github.com>
2023-03-13 07:25:01 +0000

    Tute01 done

--- Tute01.c
+++ Tute01.c
@@ -1,12 +1,20 @@
-/* Exercise 1 - Calculations
-
-   Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
-
 #include <stdio.h>
 
 // main function
 int main() {
-  
+
+  float x, y, z;
+
+  printf("Enter the marks for subject1: ");
+  scanf("%f", &x);
+
+  printf("Enter the marks for subject2: ");
+  scanf("%f", &y);
+
+  z = (x + y)/2.00;
+
+  printf("Average is %f", z);
+
   return 0;
 }
 
